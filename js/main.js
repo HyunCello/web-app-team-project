950e064290549f6d3a9cfbc7e8d95f04e3bf901a
HyunCello <jeremykim95@gmail.com>
2019-12-19 14:42:55 +0900

    add responsive, mobile toggle, random image showing in index page

--- js/main.js
+++ js/main.js
@@ -20,8 +20,22 @@ function hide(){
     document.getElementById("hideshow").style.visibility = "visible";
 }
 
+
+
+function showImage(){ 
+    var imgNum=Math.round(Math.random()*3); 
+    var objImg=document.getElementById("introimg"); 
+    objImg.src=imgArray[imgNum]; 
+    setTimeout("showImage()",3000); 
+}
+
   
-var DefineSize = window.matchMedia("(max-width: 1000px)")
+var imgArray=new Array(); 
+imgArray[0]="img/1.png"; 
+imgArray[1]="img/2.png"; 
+imgArray[2]="img/3.png"; 
+
+var DefineSize = window.matchMedia("(max-width: 1145px)")
 myFunction(DefineSize) // Call listener function at run time
 DefineSize.addListener(myFunction) // Attach listener function on state changes
 
